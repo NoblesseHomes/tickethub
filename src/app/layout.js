c8cb8665d9ad881d702819fb16caa64cde177478
Oleksandr Dyrma <noblesse.dyrma@gmail.com>
2026-05-15 08:52:43 +0200

    Обновленное - не последнее лого

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ const Header = () => {
       <div className="mx-auto flex h-16 w-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
         <Link href="/" className="flex items-center">
           <Image
-            src="/logo_ticket.svg"
+            src="/logo.svg"
             alt="TicketHub logo"
             width={196}
             height={48}
